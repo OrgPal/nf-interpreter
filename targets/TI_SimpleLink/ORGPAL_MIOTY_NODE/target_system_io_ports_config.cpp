6ffdffb0aac5d471cc9c2afbded6f3c220e9a683
José Simões <jose.simoes@eclo.solutions>
2021-10-01 00:32:28 +0100

    Rename S.IO.Portd config files

--- targets/TI_SimpleLink/ORGPAL_MIOTY_NODE/target_system_io_ports_config.cpp
+++ targets/TI_SimpleLink/ORGPAL_MIOTY_NODE/target_system_io_ports_config.cpp
@@ -3,7 +3,7 @@
 // See LICENSE file in the project root for full license information.
 //
 
-#include "target_system_io_serial_config.h"
+#include "target_system_io_ports_config.h"
 #include <sys_io_ser_native_target.h>
 
 ///////////
