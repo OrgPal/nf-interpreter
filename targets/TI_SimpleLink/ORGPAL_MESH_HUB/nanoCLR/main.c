39a589b5c34f71572ffdfb78a51c05d009145429
josesimoes <jose.simoes@eclo.solutions>
2022-02-08 14:31:11 +0000

    Add ORGPAL_MESH_HUB
    - 2 UARTs, no ADC.

---
+++ targets/TI_SimpleLink/ORGPAL_MESH_HUB/nanoCLR/main.c
@@ -0,0 +1,139 @@
+//
+// Copyright (c) 2020 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+//#include <stdint.h>
+#include <nanoCLR_Application.h>
+#include <nanoHAL_v2.h>
+
+// RTOS header files
+#include <xdc/std.h>
+#include <ioc.h>
+#include <xdc/runtime/Error.h>
+#include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Task.h>
+
+// board Header files
+//#include <ti_drivers_config.h>
+//#include <ti/drivers/Power.h>
+#include <ti/drivers/gpio/GPIOCC26XX.h>
+// clang-format off
+#include DeviceFamily_constructPath(inc/hw_prcm.h)
+#include DeviceFamily_constructPath(driverlib/sys_ctrl.h)
+// clang-format on
+
+//////////////////////////////
+
+// Stack size in bytes
+#define THREADSTACKSIZE 2048
+
+Task_Struct receiverTask;
+Task_Struct clrTask;
+
+CLR_SETTINGS clrSettings;
+
+// this define has to match the one in cpu_gpio.cpp
+#define GPIO_MAX_PINS 16
+
+// these are declared in cpu_gpio.cpp
+extern GPIO_PinConfig gpioPinConfigs[GPIO_MAX_PINS];
+extern GPIO_CallbackFxn gpioCallbackFunctions[GPIO_MAX_PINS];
+
+// this has to be define in a C file, otherwise the linker can't replace the weak one declared in the SDK driver library
+// const GPIOCC26XX_Config GPIOCC26XX_config = {
+//     .pinConfigs = (GPIO_PinConfig *)gpioPinConfigs,
+//     .callbacks = (GPIO_CallbackFxn *)gpioCallbackFunctions,
+//     .numberOfPinConfigs = GPIO_MAX_PINS,
+//     .numberOfCallbacks = GPIO_MAX_PINS,
+//     .intPriority = (~0)};
+
+extern void ReceiverThread(UArg arg0, UArg arg1);
+extern void CLRStartupThread(UArg arg0, UArg arg1);
+
+/////////////////////////////////////////////////
+
+Task_Struct testTask;
+
+void TestTaskExec(UArg arg0, UArg arg1)
+{
+    (void)arg0;
+    (void)arg1;
+
+    // loop forever
+    while (1)
+    {
+        // Allow other tasks a chance to run
+        Task_sleep(100);
+    }
+}
+
+/////////////////////////////////////////////////
+
+int main(void)
+{
+    Task_Params taskParams;
+
+    // get and store reset reason
+    // must be called before PIN_init()
+    WakeupReasonStore = SysCtrlResetSourceGet();
+
+    // Call board init functions
+    Board_init();
+
+    GPIO_init();
+    ConfigUART();
+
+    // ////////////////////////////////////////////////////////////////////
+    // *** FOR DEBUG RUNS, PLEASE KEEP THIS CODE ***
+    // // setup Test task
+    // Task_Params_init(&taskParams);
+    // taskParams.stackSize = THREADSTACKSIZE;
+    // taskParams.priority = 4;
+    // // create task
+    // Task_construct(&testTask, TestTaskExec, &taskParams, Error_IGNORE);
+    // ////////////////////////////////////////////////////////////////////
+
+    // Map LNA enable pin RFC_GPO0 to DIO21
+    IOCPortConfigureSet(IOID_21, IOC_PORT_RFC_GPO0, IOC_IOMODE_NORMAL);
+
+    // Map PA enable pin RFC_GPO1 to DIO22
+    IOCPortConfigureSet(IOID_22, IOC_PORT_RFC_GPO1, IOC_IOMODE_NORMAL);
+
+    // setup Task thread
+    Task_Params_init(&taskParams);
+    taskParams.stackSize = THREADSTACKSIZE;
+    taskParams.priority = 4;
+
+    // create Receiver
+    Task_construct(&receiverTask, ReceiverThread, &taskParams, Error_IGNORE);
+
+    // CLR settings to launch CLR thread
+    (void)memset(&clrSettings, 0, sizeof(CLR_SETTINGS));
+
+    clrSettings.MaxContextSwitches = 50;
+    clrSettings.WaitForDebugger = false;
+    clrSettings.EnterDebuggerLoopAfterExit = true;
+
+    // setup CLR task
+    taskParams.arg0 = (UArg)&clrSettings;
+    taskParams.stackSize = 2 * THREADSTACKSIZE;
+    taskParams.priority = 4;
+
+    Task_construct(&clrTask, CLRStartupThread, &taskParams, Error_IGNORE);
+
+    BIOS_start();
+
+    return (0);
+}
+
+///////////////////////////////////////////////////////////////////////
+// need this dummy implementation here (started with SDK 4.20.01.04) //
+///////////////////////////////////////////////////////////////////////
+void __attribute__((naked)) _exit(int code)
+{
+    (void)code;
+
+    for (;;)
+        ;
+}
