73f923511f9b177119972be1b2ff7b9bb0841f28
josesimoes <jose.simoes@eclo.solutions>
2022-04-21 09:16:12 +0100

    Bringing ORGPAL targets up to date with upstream

--- targets/TI_SimpleLink/ORGPAL_MESH_HUB/target_system_device_gpio.h
+++ targets/TI_SimpleLink/ORGPAL_MESH_HUB/target_system_device_gpio.h
@@ -3,10 +3,10 @@
 // See LICENSE file in the project root for full license information.
 //
 
-#ifndef _TARGET_WINDOWS_DEVICES_GPIO_H_
-#define _TARGET_WINDOWS_DEVICES_GPIO_H_
+#ifndef _TARGET_SYSTEM_DEVICE_GPIO_H_
+#define _TARGET_SYSTEM_DEVICE_GPIO_H_
 
 // max number of GPIO pins
 #define GPIO_PINS_COUNT 32
 
-#endif // _TARGET_WINDOWS_DEVICES_GPIO_H_
+#endif // _TARGET_SYSTEM_DEVICE_GPIO_H_
