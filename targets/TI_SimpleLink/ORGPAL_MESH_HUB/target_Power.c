b14f9fdc10aaebc6ce2a04212f14ee5b3ac45e83
José Simões <jose.simoes@eclo.solutions>
2023-02-15 18:05:21 +0000

    Add implementation of target low power mode setup for ORGPAL MESH

--- targets/TI_SimpleLink/ORGPAL_MESH_HUB/target_Power.c
+++ targets/TI_SimpleLink/ORGPAL_MESH_HUB/target_Power.c
@@ -4,8 +4,26 @@
 //
 
 #include <driverlib/sys_ctrl.h>
+#include <ti/drivers/Power.h>
+#include <ti/drivers/pin/PINCC26XX.h>
+#include <nanoHAL_Power.h>
 
 void CPU_Reset()
 {
     SysCtrlSystemReset();
 };
+
+// strong implementation of CPU_SetPowerModeTarget
+void CPU_SetPowerModeTarget(PowerLevel_type powerLevel)
+{
+    (void)powerLevel;
+
+    // disable UART pins and ADC
+    PIN_Config BoardGpioInitTable[] = {
+        12 | PIN_INPUT_EN | PIN_NOPULL | PIN_IRQ_DIS,
+        13 | PIN_INPUT_EN | PIN_NOPULL | PIN_IRQ_DIS,
+        24 | PIN_INPUT_EN | PIN_NOPULL | PIN_IRQ_DIS,
+        PIN_TERMINATE};
+
+    PIN_init(BoardGpioInitTable);
+}
