21d599c9f23d54fc32c6f27e8c29b40a14126c8d
josesimoes <jose.simoes@eclo.solutions>
2024-09-10 03:20:27 +0100

    Remove acc heap and allocation
    - Not required following change in no cache config for SRAM.

--- targets/ChibiOS/_nanoCLR/targetHAL_acc_integration.c
+++ targets/ChibiOS/_nanoCLR/targetHAL_acc_integration.c
@@ -13,41 +13,14 @@
 #include <acc_integration.h>
 #include <acc_integration_log.h>
 
-// Define a heap area
-#define HEAP_SIZE 2048
-uint8_t acc_heap_area[HEAP_SIZE] __attribute__((section(".accheap")));
-
-// Declare a heap object
-static memory_heap_t accHeap;
-static bool heapInitialized = false;
-
-void *acc_hal_mem_alloc(size_t size)
-{
-    if (!heapInitialized)
-    {
-        chHeapObjectInit(&accHeap, acc_heap_area, HEAP_SIZE);
-        heapInitialized = true;
-    }
-
-    // round up to nearest 32 bytes
-    size = (size + 31) & ~31;
-
-    return chHeapAllocAligned(&accHeap, size, 32);
-}
-
-void acc_hal_mem_free(void *ptr)
-{
-    chHeapFree(ptr);
-}
-
 const acc_hal_a121_t *acc_hal_rss_integration_get_implementation(void)
 {
     static const acc_hal_a121_t val = {
         // set to max uint32_t value
         .max_spi_transfer_size = 65535,
 
-        .mem_alloc = acc_hal_mem_alloc,
-        .mem_free = acc_hal_mem_free,
+        .mem_alloc = platform_malloc,
+        .mem_free = platform_free,
 
         .transfer = NULL,
         .log = acc_nano_hal_integration_log,
